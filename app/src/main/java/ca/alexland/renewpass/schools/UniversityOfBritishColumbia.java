4c759369b12499586cf693e21326cbbb59e06c89
Alex Land <aland@sfu.ca>
2016-01-14 14:45:49 -0800

    Added UBC login support

---
+++ app/src/main/java/ca/alexland/renewpass/schools/UniversityOfBritishColumbia.java
@@ -0,0 +1,39 @@
+package ca.alexland.renewpass.schools;
+
+import android.text.Html;
+
+import com.gistlabs.mechanize.document.html.HtmlDocument;
+import com.gistlabs.mechanize.document.html.form.Form;
+import com.gistlabs.mechanize.document.html.form.Password;
+import com.gistlabs.mechanize.document.html.form.Text;
+
+import ca.alexland.renewpass.exceptions.SchoolAuthenticationFailedException;
+
+/**
+ * Created by AlexLand on 2016-01-14.
+ */
+public class UniversityOfBritishColumbia implements School {
+    public final String ID = "ubc";
+
+    @Override
+    public HtmlDocument login(HtmlDocument authPage, String username, String password) throws SchoolAuthenticationFailedException {
+        Form authForm = authPage.form("loginForm");
+
+        Text usernameField = (Text) authForm.get("j_username");
+        Password passwordField = (Password) authForm.get("password");
+
+        usernameField.setValue(username);
+        passwordField.setValue(password);
+        HtmlDocument submittedPage = authForm.submit();
+        if(submittedPage.getUri().contains("https://upassbc.translink.ca")) {
+            return submittedPage;
+        }
+        else {
+            throw new SchoolAuthenticationFailedException();
+        }    }
+
+    @Override
+    public String getID() {
+        return ID;
+    }
+}
