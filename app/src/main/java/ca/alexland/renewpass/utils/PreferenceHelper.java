bc534f19caf363e4cd87386b7f40f5e982a5f639
Alex Land <aland@sfu.ca>
2016-01-15 16:54:42 -0800

    Added notification preferences, including refresh date selection. Also integrated alarm/notification code with date and notification settings.

--- app/src/main/java/ca/alexland/renewpass/utils/PreferenceHelper.java
+++ app/src/main/java/ca/alexland/renewpass/utils/PreferenceHelper.java
@@ -2,6 +2,9 @@ package ca.alexland.renewpass.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.Calendar;
 
 /**
  * Created by AlexLand on 2015-12-30.
@@ -11,14 +14,17 @@ public class PreferenceHelper {
     private static final String FIRST_RUN_PREFERENCE = "First Run";
     private static final String USERNAME_PREFERENCE = "Username";
     private static final String PASSWORD_PREFERENCE = "Password";
+    public static final String NOTIFICATION_DATE_PREFERENCE = "NotificationDate";
+    public static final String NOTIFICATIONS_ENABLED_PREFERENCE = "NotificationsEnabled";
 
     private SharedPreferences settings;
     private SharedPreferences.Editor editor;
     private KeyStoreUtil keyStoreUtil;
     private boolean keysExist;
+    private boolean preference;
 
     public PreferenceHelper(Context context) {
-        this.settings = context.getSharedPreferences(RENEWPASS_PREFERENCES, Context.MODE_PRIVATE);
+        this.settings = PreferenceManager.getDefaultSharedPreferences(context);
         this.editor = settings.edit();
     }
 
@@ -58,5 +64,24 @@ public class PreferenceHelper {
     public void setupKeys(Context context) {
         this.keyStoreUtil = new KeyStoreUtil(this.getUsername());
         keysExist = keyStoreUtil.createKeys(context);
+    }
+
+    public Calendar getDate() {
+        String dateVal = settings.getString(NOTIFICATION_DATE_PREFERENCE, "2016-01-21");
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = getDay(dateVal);
+        cal.set(year, month, day);
+        return cal;
+    }
+
+    private int getDay(String dateval) {
+        String[] pieces = dateval.split("-");
+        return (Integer.parseInt(pieces[2]));
+    }
+
+    public boolean getNotificationsEnabled() {
+        return settings.getBoolean(NOTIFICATIONS_ENABLED_PREFERENCE, false);
     }
 }
