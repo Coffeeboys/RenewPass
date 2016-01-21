dd469fcbce317add12bbde20da1e5308669cc7a8
Alex Land <aland@sfu.ca>
2016-01-21 15:00:19 -0800

    Added credits

--- app/src/main/java/ca/alexland/renewpass/SettingsActivity.java
+++ app/src/main/java/ca/alexland/renewpass/SettingsActivity.java
@@ -1,10 +1,11 @@
 package ca.alexland.renewpass;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.view.Window;
-import android.view.WindowManager;
+import android.support.v7.app.AlertDialog;
 
 public class SettingsActivity extends PreferenceActivity
 {
@@ -22,6 +23,24 @@ public class SettingsActivity extends PreferenceActivity
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            Preference credits = findPreference("Credits");
+            credits.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+                    alert.setTitle(getString(R.string.preference_credits));
+                    alert.setMessage(getString(R.string.preference_credits_description));
+
+                    alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alert.show();
+                    return true;
+                }
+            });
         }
     }
 
