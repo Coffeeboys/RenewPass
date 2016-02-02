2905e5a2eea5c08809fa15df3489431dbdc39e36
tclellan <tclellan@sfu.ca>
2016-02-02 01:27:47 -0800

    Added license info for RippleEffect

---
+++ app/src/androidTest/java/ca/alexland/renewpass/utils/CalendarUtilTest.java
@@ -0,0 +1,13 @@
+package ca.alexland.renewpass.utils;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Trevor on 1/30/2016.
+ */
+public class CalendarUtilTest extends TestCase {
+
+    public void testGetNextMonthTimeInMillis() throws Exception {
+
+    }
+}
