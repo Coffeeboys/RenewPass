8d26ae014cb3c1914b6fccfc4eae36a8eb4c9911
Alex Land <aland@sfu.ca>
2017-02-02 14:57:32 -0800

    Fixed class comment

--- app/src/main/java/ca/alexland/renewpass/utils/AlarmReceiver.java
+++ app/src/main/java/ca/alexland/renewpass/utils/AlarmReceiver.java
@@ -1,16 +1,13 @@
 package ca.alexland.renewpass.utils;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
 import ca.alexland.renewpass.AutoRenewService;
-import ca.alexland.renewpass.model.Callback;
-import ca.alexland.renewpass.model.Status;
 
 /**
- * Receives alarms from the OS for automatic renewal and package re-installation.
+ * Receives alarms from the OS for automatic renewal, boot completion, and package re-installation.
  */
 public class AlarmReceiver extends WakefulBroadcastReceiver {
     @Override
